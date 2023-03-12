729faa8bbc19d696a6dfdda03be6497c37641374
Tony Dandino <tdandino1005@gmail.com>
2023-03-12 12:39:09 -0500

    Created colums in product.js

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,7 +10,45 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
   },
+  // define a product_name column
+  product_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  // define a price column
+  price: {
+    type: DataTypes.DECIMAL,(10,2),
+    allowNull: false,
+    validate: {
+      isDecimal: true,
+    }
+    },
+  // define a stock column
+stock: {
+    type: DataTypes.INTEGER,
+  allowNull: false,
+  defaultValue: 10,
+  validate: {
+    isNumeric: true,
+}
+},
+// define a category_id column
+category_id: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+  references: {
+    model: 'category',
+    key: 'id',
+      }
+    }
+},
+
   {
     sequelize,
     timestamps: false,
