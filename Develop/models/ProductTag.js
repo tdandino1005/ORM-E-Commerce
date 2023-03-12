d15dac02b933af616645fbf01f1fc3c98e371b3c
Tony Dandino <tdandino1005@gmail.com>
2023-03-12 12:43:06 -0500

    Defined colums in ProductTag.js

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -7,7 +7,30 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
   },
+  // defined a product_id column
+  product_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'product',
+      key: 'id',
+    }
+  },
+  // defined a tag_id column
+  tag_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'tag',
+      key: 'id'
+    }
+  }
+},
+// defined configuration options
   {
     sequelize,
     timestamps: false,
