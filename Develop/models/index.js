3f4c91f1430946dc4a0fec3d4a842557d2adf723
Tony Dandino <tdandino1005@gmail.com>
2023-03-12 12:31:35 -0500

    Createed foreign key in Product model and ProductTag model

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+// Created a foreign key in the Product model
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+})
 
 // Categories have many Products
+// Created a foreign key in the Product model
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+})
 
 // Products belongToMany Tags (through ProductTag)
+// Created a foreign key in the ProductTag model
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+    foreignKey: 'tag_id',
+  })
 
 // Tags belongToMany Products (through ProductTag)
+// Created a foreign key in the ProductTag model
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+    foreignKey: 'product_id',
+  })
 
+  // Export all models
 module.exports = {
   Product,
   Category,
