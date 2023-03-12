33839759bb43d04cfc230abe36fea0651905b294
Tony Dandino <tdandino1005@gmail.com>
2023-03-12 18:25:22 -0500

    Imported categories

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -2,6 +2,10 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+// import Category model
+const {DECIMAL} = require('sequelize');
+
+
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -23,8 +27,8 @@ Product.init(
     },
     // define price column
     price: {
-      type: DataTypes.DECIMAL(10,2),
-      allowNull: false, 
+      type: DataTypes.DECIMAL,
+             allowNull: false, 
       validate: {
         isDecimal: true
       }
