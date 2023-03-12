4b3b57b5e627a60955a3f20cd112e8aea74532ff
Tony Dandino <tdandino1005@gmail.com>
2023-03-12 14:59:33 -0500

    Added comments

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -12,43 +12,42 @@ Product.init(
     // define columns
     id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-  },
-  // define a product_name column
-  product_name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  // define a price column
-  price: {
-    type: DataTypes.DECIMAL,(10,2),
-    allowNull: false,
-    validate: {
-      isDecimal: true,
-    }
+      allowNul: false,
+      primaryKey: true, 
+      autoIncrement: true, 
     },
-  // define a stock column
-stock: {
-    type: DataTypes.INTEGER,
-  allowNull: false,
-  defaultValue: 10,
-  validate: {
-    isNumeric: true,
-}
-},
-// define a category_id column
-category_id: {
-  type: DataTypes.INTEGER,
-  allowNull: false,
-  references: {
-    model: 'category',
-    key: 'id',
+    // define product_name column
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // define price column
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false, 
+      validate: {
+        isDecimal: true
+      }
+    }, 
+    // define stock column
+    stock:{
+      type: DataTypes.INTEGER,
+      allowNul: false,
+      defaultValue: 10,
+      validate:{
+        isNumeric: true,
+      }
+    },
+    // define category_id column
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'category',
+        key: 'id'
       }
     }
-},
-
+  },
   {
     sequelize,
     timestamps: false,
