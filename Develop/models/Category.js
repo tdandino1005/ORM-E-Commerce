c207bdc4842291aed27c928b8719005f4373c125
Tony Dandino <tdandino1005@gmail.com>
2023-03-12 12:24:31 -0500

    Created columns in category.js

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,6 +7,18 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    // id has to be the same as the column name in the database
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+  },
+// category name has to be the same as the column name in the database
+  category_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
   },
   {
     sequelize,
