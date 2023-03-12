65b14d183f1c4b32bcb7a54eccf129a3f685f899
Tony Dandino <tdandino1005@gmail.com>
2023-03-12 18:34:39 -0500

    changed foreign keys to make work

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,36 +1,35 @@
-// import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-// Created a foreign key in the Product model
+// Products belongTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
-})
+});
 
 // Categories have many Products
-// Created a foreign key in the Product model
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-})
+});
 
 // Products belongToMany Tags (through ProductTag)
-// Created a foreign key in the ProductTag model
 Product.belongsToMany(Tag, {
-  through: ProductTag,
-    foreignKey: 'tag_id',
-  })
+  through: {
+    model: ProductTag,
+  },
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
-// Created a foreign key in the ProductTag model
 Tag.belongsToMany(Product, {
-  through: ProductTag,
-    foreignKey: 'product_id',
-  })
+  through: {
+    model: ProductTag,
+  },
+  foreignKey: 'tag_id',
+});
 
-  // Export all models
+// Export all models
 module.exports = {
   Product,
   Category,
